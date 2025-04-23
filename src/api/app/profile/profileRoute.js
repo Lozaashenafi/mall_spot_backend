19f29ed62eeb34d82aeeb4d1b8638657fbed6e91
Lozaashenafi <134071796+Lozaashenafi@users.noreply.github.com>
2025-04-23 19:45:22 +0300

    lo

--- src/api/app/profile/profileRoute.js
+++ src/api/app/profile/profileRoute.js
@@ -10,6 +10,6 @@ const router = express.Router();
 // Register route
 router.get("/:id", getProfile);
 router.put("/update/:id", updateProfile); // Assuming you have an updateProfile function in your controller
-router.post("/:userId/changere-password", changePassword);
+router.post("/:userId/change-password", changePassword);
 
 export default router;
