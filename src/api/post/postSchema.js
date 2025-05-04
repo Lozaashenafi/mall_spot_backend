d34e1e9e3a2c531d8cbf580d96df7943257541bc
Lozaashenafi <134071796+Lozaashenafi@users.noreply.github.com>
2025-05-04 23:01:30 +0300

    run

--- src/api/post/postSchema.js
+++ src/api/post/postSchema.js
@@ -7,7 +7,6 @@ export default {
     title: Joi.string().min(3).max(100).required(),
     description: Joi.string().allow(null, "").max(500),
     price: Joi.number().positive().allow(null),
-    bidDeposit: Joi.number().positive().allow(null),
     bidEndDate: Joi.date().allow(null),
     userId: Joi.number().integer().required(),
     status: Joi.string().valid("PENDING", "INVISIBLE").optional(),
